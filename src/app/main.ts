0b0d00f1040c5538391baebde2a567aba259dab3
Julien <julien.courtial@laposte.net>
2017-12-12 16:49:53 +0100

    Shake

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,4 +4,5 @@ import { AppModule } from './app.module';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/pluck';
+import 'rxjs/add/operator/switchMap';
 platformBrowserDynamic().bootstrapModule(AppModule);
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { DetailsPage } from '../details/details';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { api_key } from '../../app/tmdb';
+import { AlertController } from 'ionic-angular';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
+import { Shake } from '@ionic-native/shake';
+import { Subscription } from 'rxjs/Subscription';
 
 export interface Result {
   title: string;
@@ -9,6 +14,7 @@ export interface Result {
   poster_path:string;
   backdrop_path:string;
   release_date:number;
+  id:number;
 
 }
 
@@ -22,9 +28,23 @@ export class HomePage {
   params:any;
   pushPage=DetailsPage;
 
-  constructor(private http : HttpClient) {
+
+
+
+  constructor(private http: HttpClient, public alertCtrl: AlertController, public navCtrl: NavController, private shake : Shake, private shakeSubscription : Subscription ) {
     this.films=null;
 
+
+  }
+
+  ionViewDidEnter() {
+    this.shakeSubscription = this.shake.startWatch()
+      .switchMap(() => this.discoverMovies())
+      .subscribe(movies => this.showRandomMovieAlert(movies));
+  }
+
+  ionViewWillLeave(){
+    this.shakeSubscription.unsubscribe();
   }
 
   getFilms(ev:any){
@@ -42,7 +62,34 @@ export class HomePage {
 
   fetchResults(query : string):Observable<Result[]>{
 
-    return  this.http.get<Result[]>("https://api.themoviedb.org/3/search/movie?api_key=ebb02613ce5a2ae58fde00f4db95a9c1&language=fr&query="+query).pluck('results');
+    return  this.http.get<Result[]>("https://api.themoviedb.org/3/search/movie?api_key="+api_key+"&language=fr-FR&query="+query).pluck('results');
   }
+
+  private discoverMovies():Observable<Result[]>{
+    return this.http.get<Result[]>("https://api.themoviedb.org/3/discover/movie?api_key="+api_key+"&language=fr-FR&primary_release_year=2018").pluck('results');
+
+  }
+
+  private showRandomMovieAlert(movies: Result[]): void{
+    let i = Math.floor(Math.random() * movies.length);
+
+    let confirm = this.alertCtrl.create({
+      title: movies[i].title,
+      message: movies[i].overview,
+      buttons: [
+        {
+          text: 'Annuler'
+        },
+        {
+          text: 'Details',
+          handler: () => {
+            this.navCtrl.push(DetailsPage, movies[i]);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
 
 }
